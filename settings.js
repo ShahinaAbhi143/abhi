3e8930a55eeb35aab25ddd9c610d68b785b41d60
ShahinaAbhi143 <nityarajak402@gmail.com>
2024-10-12 11:42:35 +0530

    WhatsApp Bot

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'My Sticker Pack',
+  author: 'My Bot',
+  botName: "Abhi Bot",
+  botOwner: 'Abhi Singh', // Your name
+  ownerNumber: '918718038836', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
